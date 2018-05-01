49c6b6165a8e6d80ed1eb2659a17a23be62b1161
thetorie <vrc7de@virginia.edu>
2018-05-01 10:47:49 -0400

    commenting out parts of the javascript that don't work for now

--- javascript/analysis.js
+++ javascript/analysis.js
@@ -275,8 +275,8 @@ function find_search_results(json_file, needleArray) {
     let url = data['response']['docs'][0]['id'];
     let size = search_results + 1;
     // this part is what I'm working on
-    var myElement = document.getElementsByClassName("pretty-text");
-    myElement.style.width = `80 px + ${size}`;
+    // var myElement = document.getElementsByClassName("pretty-text");
+    // myElement.style.width = `80 px + ${size}`;
     let html = `<a href="https://search.lib.virginia.edu/catalog/${url}" target="_blank"><li class="pretty-text">${titleText} <br> ${size}</li></a>`;
     // console.log(html);
     console.log("number of search hits: " + search_results);
